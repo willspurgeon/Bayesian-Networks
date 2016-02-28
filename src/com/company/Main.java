064ec6a26047657dca2b3c7063b3104c06a7da20
Will Spurgeon <wespurgeon@wpi.edu>
2016-02-28 17:05:37 -0500

    Main function started.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,38 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    if(args.length != 3){
+            System.out.println("Incorrect number of inputs!");
+            System.exit(-1);
+        }
+
+        String networkFilePath = args[0];
+        String queryFilePath = args[1];
+        Integer numSamples = Integer.parseInt(args[2]);
+
+
+        try {
+
+            FileReader networkFileReader = new FileReader(networkFilePath);
+            BufferedReader networkBufferedReader = new BufferedReader(networkFileReader);
+
+            FileReader queryFileReader = new FileReader(queryFilePath);
+            BufferedReader queryBufferedReader = new BufferedReader(queryFileReader);
+
+
+            int inputCount = 0;
+            bufferedReader.readLine();
+        }catch (FileNotFoundException error){
+
+        }catch (IOException error){
+
+        }
     }
 }
